d0fa50ae843f44bd82750ea62a4922bcb439f210
Amanda Oliveira <amandadias8123@gmail.com>
2022-06-15 09:40:59 -0300

    Toodoo Teste Técnico

---
+++ 11/11.js
@@ -0,0 +1,13 @@
+//Questão 11
+
+function multiplicacao (num1, num2){
+    let = num1 * num2;
+    return resultado;
+
+}
+
+console.log("O resultado da multiplicação é: " + multiplicacao(4,7)); //28
+
+console.log("O resultado da multiplicação é: " + multiplicacao(5,6)); // 30
+
+console.log("O resultado da multiplicação é: " + multiplicacao(3,3)); // 9
